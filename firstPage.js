cfa2dc5052821909209bdc92e7b9e6cb3c15f65d
Rohan Sharma <specialrohansharma200@gmail.com>
2024-04-03 21:39:11 +0530

    js

---
+++ firstPage.js
@@ -0,0 +1,55 @@
+//Changing image
+// function hover(element) {
+//     element.setAttribute('src', 'Assets/sad.gif');
+// }
+  
+// function unhover(element) {
+//     element.setAttribute('src', 'Assets/hi.gif');
+// }
+
+
+
+//Changing background
+// var button = document.getElementById('hover');
+// var body = document.body;
+
+// button.onmouseover = function() {
+// 	body.className = 'hovered';
+// }
+
+// button.onmouseout = function() {
+// 	body.className = '';
+// }
+
+
+//Changing gif
+const img = document.getElementById('image');
+
+const images = {
+    hi: "Assets/hi.gif",
+    ori: "Assets/sad.gif"
+}
+
+function ChangeImage(giphy) {
+    img.src = images[giphy];
+}
+function ResetImage() {
+    img.src = images.ori;
+};
+
+
+//Yes button bg image
+var body = document.body;
+
+function bg() {
+	body.className = 'hovered';
+};
+
+function resetBG(){
+	body.className = '';
+};
+
+//Linking to second page
+function myFunction() {
+    window.location.href = "Pages/Page 1/secondPage.html";
+}
